1575249f3dda5f330a844ac9f280c8d6d583be49
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-12-01 12:51:30 +0900

    Update kakakoMap.jsx

--- src/pages/Home/components/kakakoMap.jsx
+++ src/pages/Home/components/kakakoMap.jsx
@@ -1,6 +1,5 @@
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Main from 'assets/image/marker.PNG';
-import Parking from 'assets/image/parking.png';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 
 export default function KakaoMap() {
@@ -16,12 +15,6 @@ export default function KakaoMap() {
         clickable
         onClick={() => window.open('https://map.naver.com/p/entry/place/1669555615?c=15.00,0,0,0,dh')}
         image={{ src: Main, size: { width: 150, height: '100%' }, options: { offset: { x: 75, y: 55 } } }}
-      />
-      <MapMarker
-        position={{ lat: 37.453887063036525, lng: 126.90161879243772 }}
-        clickable
-        onClick={() => window.open('https://map.naver.com/p/entry/place/427542720?c=18.79,0,0,0,dh')}
-        image={{ src: Parking, size: { width: 50, height: '100%' }, options: { offset: { x: 25, y: 55 } } }}
       />
     </Map>
   );
