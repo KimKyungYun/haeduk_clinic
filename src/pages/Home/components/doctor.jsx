9103beeb6a40d36b7289e8b7f374f8a87d2fd995
KimKyungYun <kky38225221@gmail.com>
2024-03-20 14:22:53 +0900

    feat: 의사이름 수정

--- src/pages/Home/components/doctor.jsx
+++ src/pages/Home/components/doctor.jsx
@@ -2,7 +2,7 @@ import Doctor3 from 'assets/image/doctor/doctor3.png';
 import Doctor2 from 'assets/image/doctor/doctor2.png';
 import Doctor1 from 'assets/image/doctor/doctor1.png';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
-import CAREER from 'constants/doctor_career';
+import DOCTOR_INFO from 'constants/doctor_career';
 import styles from '../Home.module.scss';
 
 export default function Doctor() {
@@ -15,89 +15,92 @@ export default function Doctor() {
           <div className={styles.doc__career}>
             <div className={styles['doc__career--top']}>
               <span className={styles['doc__career--name']}>
-                김혜인
+                {DOCTOR_INFO.first.name}
                 {' '}
               </span>
               <span className={styles['doc__career--rank']}>대표원장</span>
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              {CAREER.first.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
+              {DOCTOR_INFO.first.career.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
             <img src={Doctor1} alt="chief" />
           </div>
           <div className={styles.doc__name}>
-            <span className={styles['doc__name--name']}>김혜인</span>
+            <span className={styles['doc__name--name']}>{DOCTOR_INFO.first.name}</span>
             대표원장
           </div>
         </div>
+
         <div className={styles.doc__wrap}>
           <div className={styles.doc__career}>
             <div className={styles['doc__career--top']}>
               <span className={styles['doc__career--name']}>
-                최덕주
+                {DOCTOR_INFO.second.name}
                 {' '}
               </span>
               <span className={styles['doc__career--rank']}>진료원장</span>
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              {CAREER.second.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
+              {DOCTOR_INFO.second.career.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
             <img style={{ height: '90%' }} src={Doctor2} alt="doc" />
           </div>
           <div className={styles.doc__name}>
-            <span className={styles['doc__name--name']}>최덕주</span>
+            <span className={styles['doc__name--name']}>{DOCTOR_INFO.second.name}</span>
             진료원장
           </div>
         </div>
+
         {!isMobile && (
         <div className={styles.doc__wrap}>
           <div className={styles.doc__career}>
             <div className={styles['doc__career--top']}>
               <span className={styles['doc__career--name']}>
-                김혜인
+                {DOCTOR_INFO.first.name}
                 {' '}
               </span>
               <span className={styles['doc__career--rank']}>대표원장</span>
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              {CAREER.first.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
+              {DOCTOR_INFO.first.career.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
             <img src={Doctor1} alt="chief" />
           </div>
           <div className={styles.doc__name}>
-            <span className={styles['doc__name--name']}>김혜인</span>
+            <span className={styles['doc__name--name']}>{DOCTOR_INFO.first.name}</span>
             대표원장
           </div>
         </div>
         )}
+
         <div className={styles.doc__wrap}>
           <div className={styles.doc__career}>
             <div className={styles['doc__career--top']}>
               <span className={styles['doc__career--name']}>
-                박영하
+                {DOCTOR_INFO.third.name}
                 {' '}
               </span>
               <span className={styles['doc__career--rank']}>진료원장</span>
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              {CAREER.third.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
+              {DOCTOR_INFO.third.career.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
             <img src={Doctor3} alt="doc" />
           </div>
           <div className={styles.doc__name}>
-            <span className={styles['doc__name--name']}>박영하</span>
+            <span className={styles['doc__name--name']}>{DOCTOR_INFO.third.name}</span>
             진료원장
           </div>
         </div>
