cf6e4f35f1a8bb4ab40d9941e644e095bc646e0a
KimKyungYun <kky38225221@gmail.com>
2024-03-20 13:33:25 +0900

    feat: 최덕주 수정

--- src/pages/Home/components/doctor.jsx
+++ src/pages/Home/components/doctor.jsx
@@ -1,7 +1,8 @@
-import Youngha from 'assets/image/doctor/youngha.png';
-import HeeWon from 'assets/image/doctor/heewon.png';
-import Hyein from 'assets/image/doctor/hyein.png';
+import Doctor3 from 'assets/image/doctor/doctor3.png';
+import Doctor2 from 'assets/image/doctor/doctor2.png';
+import Doctor1 from 'assets/image/doctor/doctor1.png';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
+import CAREER from 'constants/doctor_career';
 import styles from '../Home.module.scss';
 
 export default function Doctor() {
@@ -21,21 +22,11 @@ export default function Doctor() {
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              <li className={styles['doc__career--list']}>현) 해득금천한의원 대표원장</li>
-              <li className={styles['doc__career--list']}>전) 광덕안정한의원 일산주엽점 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 미올한의원 파주운정점 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 누베베한의원 강남점 진료원장</li>
-              <li className={styles['doc__career--list']}>원광대학교 한의과대학 졸업</li>
-              <li className={styles['doc__career--list']}>한방비만학회 회원</li>
-              <li className={styles['doc__career--list']}>대한약침학회 회원</li>
-              <li className={styles['doc__career--list']}>대한한방부인과학회 회원</li>
-              <li className={styles['doc__career--list']}>척추신경추나의학회 회원</li>
-              <li className={styles['doc__career--list']}>대한한의학회 회원</li>
-              <li className={styles['doc__career--list']}>턱관절균형의학회 FCST 일반과정 수료</li>
+              {CAREER.first.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
-            <img src={Hyein} alt="chief" />
+            <img src={Doctor1} alt="chief" />
           </div>
           <div className={styles.doc__name}>
             <span className={styles['doc__name--name']}>김혜인</span>
@@ -46,29 +37,21 @@ export default function Doctor() {
           <div className={styles.doc__career}>
             <div className={styles['doc__career--top']}>
               <span className={styles['doc__career--name']}>
-                최희원
+                최덕주
                 {' '}
               </span>
               <span className={styles['doc__career--rank']}>진료원장</span>
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              <li className={styles['doc__career--list']}>현) 해득금천한의원 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 북가좌경희한의원 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 금맥한의원 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 누베베한의원 부산점 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 누베베한의원 홍대점 진료원장</li>
-              <li className={styles['doc__career--list']}>동의대학교 한의과대학 졸업</li>
-              <li className={styles['doc__career--list']}>척추신경추나의학회 정회원</li>
-              <li className={styles['doc__career--list']}>한방비만학회 회원</li>
-              <li className={styles['doc__career--list']}>대한한의학회 회원</li>
+              {CAREER.second.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
-            <img style={{ height: '90%' }} src={HeeWon} alt="doc" />
+            <img style={{ height: '90%' }} src={Doctor2} alt="doc" />
           </div>
           <div className={styles.doc__name}>
-            <span className={styles['doc__name--name']}>최희원</span>
+            <span className={styles['doc__name--name']}>최덕주</span>
             진료원장
           </div>
         </div>
@@ -84,21 +67,11 @@ export default function Doctor() {
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              <li className={styles['doc__career--list']}>현) 해득금천한의원 대표원장</li>
-              <li className={styles['doc__career--list']}>전) 광덕안정한의원 일산주엽점 원장</li>
-              <li className={styles['doc__career--list']}>전) 미올한의원 파주운정점 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 누베베한의원 강남점 진료원장</li>
-              <li className={styles['doc__career--list']}>원광대학교 한의과대학 졸업</li>
-              <li className={styles['doc__career--list']}>한방비만학회 회원</li>
-              <li className={styles['doc__career--list']}>대한약침학회 회원</li>
-              <li className={styles['doc__career--list']}>대한한방부인과학회 회원</li>
-              <li className={styles['doc__career--list']}>척추신경추나의학회 회원</li>
-              <li className={styles['doc__career--list']}>대한한의학회 회원</li>
-              <li className={styles['doc__career--list']}>턱관절균형의학회 FCST 일반과정 수료</li>
+              {CAREER.first.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
-            <img src={Hyein} alt="chief" />
+            <img src={Doctor1} alt="chief" />
           </div>
           <div className={styles.doc__name}>
             <span className={styles['doc__name--name']}>김혜인</span>
@@ -117,19 +90,11 @@ export default function Doctor() {
               <span>한의사</span>
             </div>
             <ul className={styles['doc__career--ul']}>
-              <li className={styles['doc__career--list']}>현) 해득금천한의원 진료원장</li>
-              <li className={styles['doc__career--list']}>전) 부야한의원 부원장</li>
-              <li className={styles['doc__career--list']}>전) 광덕안정한의원 영등포대림점 진료원장</li>
-              <li className={styles['doc__career--list']}>동신대학교 한의과대학 졸업</li>
-              <li className={styles['doc__career--list']}>척추신경추나학회 정회원</li>
-              <li className={styles['doc__career--list']}>척추신경추나학회 정규워크샵 수료</li>
-              <li className={styles['doc__career--list']}>한의기능영양학회 정회원</li>
-              <li className={styles['doc__career--list']}>대한동의방약학회 회원</li>
-              <li className={styles['doc__career--list']}>심리상담사 2급</li>
+              {CAREER.third.map((data) => <li className={styles['doc__career--list']} key={data}>{data}</li>)}
             </ul>
           </div>
           <div className={styles.doc__img}>
-            <img src={Youngha} alt="doc" />
+            <img src={Doctor3} alt="doc" />
           </div>
           <div className={styles.doc__name}>
             <span className={styles['doc__name--name']}>박영하</span>
